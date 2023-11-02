7cdf8408a08ad53f7f0c51fa83b71143f7f2d824
sartfromua <sartfromua@gmail.com>
2023-11-02 21:07:59 +0200

    Added MoreInfoActivity, PhoneIntent. Created EmailIntent (TODO).

--- app/src/main/java/com/example/cvapp/MainActivity.kt
+++ app/src/main/java/com/example/cvapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.cvapp
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -17,9 +19,28 @@ class MainActivity : AppCompatActivity() {
         val email = findViewById<TextView>(R.id.textEmail)
         val picture = findViewById<ImageView>(R.id.pictureImageView)
         val shortInfo = findViewById<TextView>(R.id.textInfoShort)
+
         picture.setOnClickListener {
-            val intent = Intent(applicationContext, BigPhoto::class.java)
-            startActivity(intent)
+            startActivity(Intent(applicationContext, BigPhoto::class.java))
+        }
+        shortInfo.setOnClickListener {
+            startActivity(Intent(applicationContext, MoreInfoActivity::class.java))
+        }
+        phone.setOnClickListener {
+            val number = getString(R.string.phoneNumber)
+            startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:${number}")))
+        }
+        email.setOnClickListener {
+            val mail = getString(R.string.email)
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:${mail}")
+                    .buildUpon()
+                    .appendQueryParameter("Subject", "saf").
+                    appendQueryParameter("mailto", mail)
+                        .build()
+//                putExtra(Intent.EXTRA_SUBJECT, "Invitation to participate in the interview")
+            }
+            startActivity(Intent.createChooser(intent, "Choose Email Client..."))
         }
 
     }
--- app/src/main/java/com/example/cvapp/MoreInfoActivity.kt
+++ app/src/main/java/com/example/cvapp/MoreInfoActivity.kt
@@ -1,17 +1,17 @@
 package com.example.cvapp
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.ImageView
 
-class BigPhoto : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class MoreInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_big_photo)
-        findViewById<ImageView>(R.id.imageBackPhoto).setOnClickListener {
+        setContentView(R.layout.activity_more_info)
+        findViewById<ImageView>(R.id.imageBackInfo).setOnClickListener{
             startActivity(Intent(applicationContext, MainActivity::class.java))
         }
     }
